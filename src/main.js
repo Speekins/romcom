7ea3cc8e54c16957425d91dc9a6049a6a1087e54
Spencer Haka <spencer.haka@gmail.com>
2022-09-02 10:24:00 -0600

    add ability to create new cover

--- src/main.js
+++ src/main.js
@@ -21,23 +21,40 @@ var userDesc2 = document.querySelector('.user-desc2');
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
+var currentCover;
+var newUserCover;
 
 // Add your event listeners here 👇 (should go at the bottom)
+
 window.addEventListener('load', newRandomCover)
-randomButton.addEventListener('click', newRandomCover)
-makeNewButton.addEventListener('click', makeYourOwn)
-viewSavedButton.addEventListener('click', viewSavedCovers)
-homeButton.addEventListener('click', goHome)
-createNewBookButton.addEventListener('click', getNewBookData)
+randomButton.addEventListener('click', newRandomCover);
+makeNewButton.addEventListener('click', makeYourOwn);
+viewSavedButton.addEventListener('click', viewSavedCovers);
+homeButton.addEventListener('click', goHome);
+createNewBookButton.addEventListener('click', getNewBookData);
+
 // Create your event handlers and other functions here 👇
+
 function newRandomCover() {
-  currentCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)],
-    descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
-    coverImage.src = currentCover.cover;
-    title.innerText = currentCover.title;
-    descriptor1.innerText = currentCover.tagline1;
-    descriptor2.innerText = currentCover.tagline2;
+  var randomCover = new Cover(covers[getRandomIndex(covers)], titles[getRandomIndex(titles)],
+  descriptors[getRandomIndex(descriptors)], descriptors[getRandomIndex(descriptors)]);
+
+  coverImage.src = randomCover.cover;
+  title.innerText = randomCover.title;
+  descriptor1.innerText = randomCover.tagline1;
+  descriptor2.innerText = randomCover.tagline2;
+
+  currentCover = randomCover;
+  displayNewCover();
 }
+
+function displayNewCover() {
+  coverImage.src = currentCover.cover;
+  title.innerText = currentCover.title;
+  descriptor1.innerText = currentCover.tagline1;
+  descriptor2.innerText = currentCover.tagline2;
+}
+
 function makeYourOwn() {
   randomButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
@@ -46,6 +63,7 @@ function makeYourOwn() {
   formView.classList.remove('hidden');
   savedView.classList.add('hidden');
 }
+
 function viewSavedCovers() {
   homeView.classList.add('hidden');
   formView.classList.add('hidden');
@@ -54,6 +72,7 @@ function viewSavedCovers() {
   saveCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
 }
+
 function goHome() {
   homeView.classList.remove('hidden');
   formView.classList.add('hidden');
@@ -64,14 +83,21 @@ function goHome() {
   viewSavedButton.classList.remove('hidden');
   makeNewButton.classList.remove('hidden');
 }
-function getNewBookData() {
-  currentCover = new Cover(userCover.value, userTitle.value, userDesc1.value, userDesc2.value);
-  covers.push(userCover.value);
-  titles.push(userTitle.value);
-  descriptors.push(userDesc1.value, userDesc2.value);
-  goHome();
 
+function getNewBookData(event) {
+  event.preventDefault();
+
+  newUserCover = new Cover(userCover.value, userTitle.value, userDesc1.value, userDesc2.value);
+
+  covers.push(newUserCover.cover);
+  titles.push(newUserCover.title);
+  descriptors.push(newUserCover.tagline1, newUserCover.tagline2);
+
+  currentCover = newUserCover;
+  goHome();
+  displayNewCover();
 }
+
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
